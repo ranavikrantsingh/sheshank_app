660f66ca3dfd4ec1412277095b62ddb17b97ec40
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-12-21 02:39:38 +0530

    changes done

--- src/screens/MailDetailsScreen.js
+++ src/screens/MailDetailsScreen.js
@@ -1,18 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const MailDetailsScreen = (props) => {
-    const {data} = props?.route?.params
-    console.log('====================================');
-    console.log(data);
-    console.log('====================================');
+import {StyleSheet, Text, SafeAreaView, View} from 'react-native';
+import React from 'react';
+import {scale} from '../utils/Scaling';
+const MailDetailsScreen = props => {
+  const {data} = props?.route?.params;
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
   return (
-    <View>
-      <Text>MailDetailsScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={styles?.margins}>
+        {data && (
+          <>
+            <Text style={styles?.boldText}>{data?.name}</Text>
+            <Text style={styles?.mediumText}>{data?.email}</Text>
+            <Text style={styles?.regularText}>{data?.message}</Text>
+          </>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default MailDetailsScreen
+export default MailDetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  margins: {
+    margin: scale(20),
+  },
+  boldText: {
+    fontSize: scale(20),
+    fontWeight: 'bold',
+  },
+  mediumText: {
+    fontSize: scale(15),
+    fontWeight: '500',
+  },
+  regularText: {
+    fontSize: scale(15),
+  },
+});
