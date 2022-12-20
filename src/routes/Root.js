571c8b67a306ff395af86fbf551a165d15165059
Rana Vikrant Singh Rathod <ranavikrantsinghrathod@Ranas-MacBook-Pro.local>
2022-12-20 23:44:54 +0530

    changes done

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,49 +1,10 @@
-// import React, {Component, useEffect} from 'react';
-// import {connect} from 'react-redux';
-// import Routes from './Routes';
-// import LoginRoutes from './LoginRoutes';
-// import {setDeviceId} from '../store/actions';
-
-// class Root extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       deviceInfo: {},
-//       isFetchingApiUrl: false,
-//     };
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {}
-
-//   render() {
-//     if (this.props.isAuthenticated) {
-//       return <Routes />;
-//     } else {
-//       return <LoginRoutes />;
-//     }
-//   }
-// }
-
-// const mapStateToProps = state => ({
-//   deviceId: state.appReducer.deviceId,
-//   isAuthenticated: state.appReducer.isAuthenticated,
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   setDeviceId: deviceId => {
-//     dispatch(setDeviceId(deviceId));
-//   },
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Root);
-
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {connect} from 'react-redux';
 import Routes from './Routes';
 import LoginRoutes from './LoginRoutes';
 const Root = props => {
-  if (props.isAuthenticated) {
+  if (props?.isAuthenticated) {
     return <Routes />;
   } else {
     return <LoginRoutes />;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,55 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import React from 'react';
-
-const HomeScreen = () => {
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { scale } from '../utils/Scaling'
+import ButtonComponent from '../components/ButtonComponent'
+import { logout } from '../store/actions'
+import { useDispatch } from 'react-redux'
+const HomeScreen = (props) => {
+  const dispatch = useDispatch()
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={styles?.mainContainer}>
+      <View style={styles?.margins}>
+        <View style={styles?.body}>
+          <Text style={styles.boldText}>Home Screen</Text>
+          <ButtonComponent
+            name="Logout"
+            onPress={() => {
+            dispatch(logout())
+            }}
+            
+          />
+        </View>
+      </View>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  margins: {
+    marginHorizontal: scale(20),
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  body: {
+    marginTop: '40%',
+  },
+  error: {
+    color: 'red',
+    fontSize: scale(12),
+    marginVertical: scale(5),
+  },
+  boldText: {
+    fontSize: scale(20),
+    fontWeight: 'bold',
+    marginVertical: scale(5),
+  },
+  mediumText: {
+    fontSize: scale(13),
+    fontWeight: 'normal',
+    marginVertical: scale(2),
+  },
+})
